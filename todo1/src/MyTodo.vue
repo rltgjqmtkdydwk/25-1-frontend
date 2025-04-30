7481e89c338c748b9de2075bd9d309f71fc708cb
rltgjqmtkdydwk <slvrcods22@gmail.com>
2025-04-30 14:05:42 +0900

    추가 기능

--- todo1/src/MyTodo.vue
+++ todo1/src/MyTodo.vue
@@ -1,9 +1,20 @@
 <script setup>
-const todos = [
+import { ref } from 'vue';
+
+const todos = ref([
   { id: 1, title: "과제" },
   { id: 2, title: "시험공부" },
   { id: 3, title:"코딩훈련" }
-]
+]);
+
+const title = ref('');
+
+function addTodo() {
+  const lastId = todos.value.length == 0 ? 0 :
+    todos.value[todos.value.length - 1].id;
+  todos.value.push({id: lastId + 1, title: title.value});
+  title.value = "";
+}
 </script>
 
 <template>
@@ -20,8 +31,9 @@ const todos = [
         </tr>
       </tbody>
     </table>
+    <input type="text" v-model.trim="title" />
+    <button type="button" v-on:click="addTodo">추가</button>
   </div>
-
 </template>
 
 <style scoped>
@@ -30,4 +42,6 @@ table { border-collapse: collapse; margin: 20px 0; width: 100%; }
 thead { background-color: #eee; text-align: center; }
 td { border: 1px solid gray; padding: 6px; }
 td:nth-child(1) { text-align: center; width: 30px; }
+input[type=text] { padding: 5px; margin-right: 5px; width: 300px; }
+button { padding: 0.3em 1.5em; }
 </style>
