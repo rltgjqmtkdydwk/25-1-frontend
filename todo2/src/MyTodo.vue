df1c7d87d74ef157493be3867a514b2e6a9ba28f
rltgjqmtkdydwk <slvrcods22@gmail.com>
2025-05-12 15:58:36 +0900

    feat: 할일 추가 폼 숨기기 할일 목록만 보기 가능

--- todo2/src/MyTodo.vue
+++ todo2/src/MyTodo.vue
@@ -8,6 +8,7 @@ const progress = ref(0); // 진척도 입력
 const editId = ref(0);   // 수정할 항목의 id
 
 const KEY = "todo2.todos"; // localStorage 저장 키
+const showAddForm = ref(false); // 입력 폼 표시 여부
 
 onMounted(() => load());
 
@@ -20,6 +21,7 @@ function addTodo() {
 
   title.value = "";
   progress.value = 0;
+  showAddForm.value = false;
   save();
 }
 
@@ -55,21 +57,24 @@ function cancel() { // 수정 취소 함수
     <h1>할 일</h1>
     <table>
       <thead>
-        <tr><td>ID</td><td>기한</td><td>진척도</td><td>할일</td></tr>
+        <tr><td>ID</td><td>기한</td><td>진척도</td><td>할일</td>
+            <td v-if="todos.length > 0 && !showAddForm"><button @click="showAddForm = true">!</button></td>
+        </tr>
       </thead>
       <tbody>
-        <tr v-for="todo in todos" v-bind:key="todo.id">
-        <template v-if="editId != todo.id">
-          <td>{{ todo.id }}</td>
-          <td>{{ todo.due }}</td>
-          <td>{{ todo.progress }}</td>
-          <td>{{ todo.title }}</td>
-          <td>
-            <button @click="editId = todo.id" :disabled="editId > 0" title="수정">e</button>
-            <button @click="deleteTodo(index)" :disabled="editId > 0" title="삭제">-</button>
-          </td>
-        </template>
-        <template v-else>
+        <!-- 목록 출력 -->
+        <tr v-for="(todo, index) in todos" :key="todo.id">
+            <template v-if="editId != todo.id">
+            <td>{{ todo.id }}</td>
+            <td>{{ todo.due }}</td>
+            <td>{{ todo.progress }}</td>
+            <td>{{ todo.title }}</td>
+            <td>
+                <button @click="editId = todo.id" :disabled="editId > 0" title="수정">e</button>
+                <button @click="deleteTodo(index)" :disabled="editId > 0" title="삭제">-</button>
+            </td>
+            </template>
+            <template v-else>
             <td>{{ todo.id }}</td>
             <td><input type="date" v-model="todo.due" /></td>
             <td><input type="number" v-model="todo.progress" step="10" /></td>
@@ -78,17 +83,22 @@ function cancel() { // 수정 취소 함수
                 <button @click="updateTodo()" class="sm">저장</button>
                 <button @click="cancel()" class="sm">취소</button>
             </td>
-        </template>
+            </template>
         </tr>
-        <tr v-if="todos.length == 0">
-          <td></td><td>할 일이 없습니다</td>
+
+        <!-- 할 일 없음 -->
+        <tr v-if="todos.length == 0 && !showAddForm">
+            <td colspan="4">할 일이 없습니다</td>
+            <td><button @click="showAddForm = true">!</button></td>
         </tr>
-        <tr>
-          <td></td>
-          <td><input type="date" v-model="due" :disabled="editId > 0" /></td>
-          <td><input type="number" v-model="progress" step="10" :disabled="editId > 0" /></td>
-          <td><input type="text" v-model.trim="title" :disabled="editId > 0" /></td>
-          <td><button @click="addTodo()" title="저장" :disabled="editId > 0" >+</button></td>
+
+        <!-- 입력 폼 표시 (todos 유무 상관없이 showAddForm true면 표시됨) -->
+        <tr v-show="showAddForm">
+            <td></td>
+            <td><input type="date" v-model="due" :disabled="editId > 0" /></td>
+            <td><input type="number" v-model="progress" step="10" :disabled="editId > 0" /></td>
+            <td><input type="text" v-model.trim="title" :disabled="editId > 0" /></td>
+            <td><button @click="addTodo()" :disabled="editId > 0">+</button></td>
         </tr>
       </tbody>
     </table>
